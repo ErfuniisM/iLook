518fe0409331ba621a057f2dc79293b66e8e88ee
Erfan <erfuniism@gmail.com>
2023-03-26 19:31:23 +0100

    Created Player.jsx and Poster Folder

---
+++ src/assets/Components/Player/Player.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Plyr from "plyr-react";
+import "plyr-react/plyr.css";
+import { Box } from "@mui/system";
+
+const plyrOptions = {
+  fullscreen: {
+    enabled: true,
+    fallback: true,
+    iosNative: true,
+  },
+};
+
+function Player({ url, provider }) {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        width: {
+          xxs: 0,
+          xs: 350, //0
+          sm: 600, // 600
+          md: 900, // 900
+          lg: 1200, // 1200
+          xl: 1536, // 1536
+        },
+      }}
+    >
+      <Plyr
+        options={plyrOptions}
+        source={{
+          poster: "src/assets/Components/Player/Posters/MovieTime.jpg",
+          type: "video",
+          sources: [
+            {
+              src: url,
+              type: "video/mp4",
+              provider: provider,
+            },
+          ],
+        }}
+      />
+    </Box>
+  );
+}
+
+export default React.memo(Player);
