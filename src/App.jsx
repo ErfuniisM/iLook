55fde5fcb3b6212804c78307a68501b55169385a
Erfan <erfuniism@gmail.com>
2023-03-26 19:43:07 +0100

    add LinkField

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   };
   return (
     <Box>
-      <DarkMode />;
+      <DarkMode LinkField={<Link fetchVideo={fetchVideo} />} />;
       <Player />
       <CharcterGif />
     </Box>
