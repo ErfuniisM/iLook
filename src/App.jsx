693f78cda027dd8c94db64bc51beda483c85c378
Erfan <erfuniism@gmail.com>
2023-03-26 19:37:21 +0100

    import LinkField, Player , CharcterGif and Box in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import DarkMode from "./assets/Components/DarkMode/DarkMode";
-
+import LinkField from "./assets/Components/LinkField/LinkField";
+import Player from "./assets/Components/Player/Player";
+import CharcterGif from "./assets/Components/RandomCharcterGif/CharcterGif";
+import { Box } from "@mui/material";
 function App() {
   return <DarkMode />;
 }
