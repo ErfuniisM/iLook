af2dd1a1484df6be5371ba3dbb61f455ea069a2e
ErfunisM <erfuniism@gmail.com>
2023-09-13 23:46:52 +0330

    change type video to mkv

--- src/assets/Components/Player/Player.jsx
+++ src/assets/Components/Player/Player.jsx
@@ -44,7 +44,7 @@ function Player({ url, provider }) {
           sources: [
             {
               src: url,
-              type: "video/mp4",
+              type: "video/mkv",
               provider: provider,
             },
           ],
